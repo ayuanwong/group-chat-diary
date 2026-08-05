bf3586c1952a966d7a9b5c3af5f173724dc32d02
Mingshi wang <mingshiwang@Mac.lan>
2026-08-06 01:05:42 +0800

    Deploy protected archive through Cloudflare Pages

--- worker/env.d.ts
+++
@@ -1,8 +0,0 @@
-interface Env {
-  /** Encrypted Cloudflare Worker secret. */
-  GITHUB_CLIENT_ID: string;
-  /** Encrypted Cloudflare Worker secret. */
-  GITHUB_CLIENT_SECRET: string;
-  /** Encrypted Cloudflare Worker secret used only for HMAC signing. */
-  SESSION_SECRET: string;
-}
